730c0e288899591892ee2bb1656e50ad712b192e
Suman Bandita <sumanbandita@gmail.com>
2018-07-31 15:13:04 -0400

    height

---
+++ height.js
@@ -0,0 +1,18 @@
+'use strict';
+const BST = require('./bst.js');
+const binary = new BST(3,3,null);  
+binary.insert(1,1);
+binary.insert(4,4);
+binary.insert(6,6);
+binary.insert(7,7);
+
+
+function height(node){ 
+  if(!node){
+    return 0; 
+  }   
+  let leftHeight = height(node.left);    
+  let rightHeight = height(node.right);  
+  return Math.max(leftHeight, rightHeight) + 1;
+}
+console.log(height(binary));
--- index.js
+++ index.js
@@ -3,20 +3,17 @@
 const BST = require('./bst.js');
 
 function main(){
-
-  const Binary = new BST();
-
-  Binary.insert(3);
-  Binary.insert(1);
-  Binary.insert(4);
-  Binary.insert(6);
-  Binary.insert(9);
-  Binary.insert(2);
-  Binary.insert(5);
-  Binary.insert(7);
-
-
-console.log(Binary);
+  const Binary = new BST(3,3,null);  
+  Binary.insert(1,1);
+  Binary.insert(4,4);
+  Binary.insert(6,6);
+  Binary.insert(9,9);
+  Binary.insert(2,2);
+  Binary.insert(5,5);
+  Binary.insert(7,7);
+  console.log(Binary);  
 }
 
 main();
+
+
